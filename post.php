2b77bd2ec671876b4df5f106924df7d142028398
Mateusz <gorzanski.mateusz@gmail.com>
2017-03-18 18:07:11 +0100

    Uzupełniono strukturę posta

--- post.php
+++ post.php
@@ -1,26 +1,31 @@
 <?php include 'header.php'; ?>
-    <div class="container">
+    <div class="container-post">
         <div class="page-content">
             <section class="main">
                 <article class="post-full">
-                    <section class="post-thumbnail-full">
+                    <div class="post-thumbnail-full">
                         <img src="images/thumbnails/1500x400/image1.jpg" alt="" />
-                    </section>
-                    <section class="post-content-full">
-
-                    </section>
-                    <section class="post-footer">
-                        <div class="row">
-                            <div class="box">
-                                <div class="post-author-box col-md-6">
-
-                                </div>
-                                <div class="post-info-box col-md-6">
-
-                                </div>
-                            </div>
+                        <div class="post-inner-title">
+                            <h2 class="post-title">Tytuł posta Tytuł posta Tytuł posta Tytuł posta Tytuł posta Tytuł posta Tytuł posta Tytuł posta Tytuł posta </h2>
                         </div>
-                    </section>
+                        <div class="post-inner-date">
+                            <span class="post-date">04.03.2017</span>
+                        </div>   
+                    </div>
+                    <div class="post-header">
+                        <p class="post-author"><a href="#">Mateusz Górzański</a></p>
+                        <p class="post-category"><a href="#">Technologia</a></p>
+                        <p class="post-comments-bt"><a href="#comments">Komentarze</a></p>
+                    </div>
+                    <div class="post-content-full">
+                        <p>Lorem ipsum dolor sit amet ligula. Suspendisse ultricies viverra neque, congue porta, turpis semper quis, accumsan tincidunt. Nullam sapien. Curabitur eget lectus. Vestibulum nibh. Maecenas nec massa non ante ipsum dolor urna eros nisl, blandit id, congue eleifend. Sed adipiscing ornare. Nullam sit amet mauris tincidunt wisi vulputate faucibus. Sed condimentum nulla. Nullam volutpat, libero quis turpis. Praesent et magnis dis parturient montes, nascetur ridiculus mus. Nam scelerisque odio consequat lorem dapibus vitae, ultricies porta scelerisque, ante vitae arcu nec felis neque, a odio vitae massa vel lorem. Etiam vel felis ut quam. Fusce ullamcorper. Nam mattis, magna diam, tincidunt justo. Praesent consequat. Morbi urna fringilla mollis. Etiam nibh. Fusce ullamcorper. Mauris aliquet malesuada, arcu erat, fringilla condimentum, dapibus tellus. Proin nonummy velit justo consequat eu, tempus rutrum id, felis. Duis a nisl. Nam consectetuer congue arcu sit amet, massa. Proin cursus wisi, fermentum eget, eros. Suspendisse eu lectus. Ut bibendum varius ac, auctor varius, nisl felis in felis tincidunt wisi, id felis. Nulla facilisi. Nullam sit amet leo sodales felis, in metus. Nulla vitae fringilla enim. Etiam rutrum. In vitae augue. Maecenas ut nibh. Maecenas in tortor massa et ultrices libero et commodo pede quis nibh. Morbi accumsan odio sit amet, consectetuer adipiscing wisi adipiscing eget, sollicitudin non, leo. Nulla ante. Quisque in faucibus eu, sagittis eleifend. Sed metus. Sed lobortis urna. Vestibulum ante mollis neque auctor odio. Nunc vehicula. Vivamus malesuada ultricies pretium. Vestibulum ante ipsum sit amet, consectetuer tortor. Nunc consectetuer adipiscing non, arcu. Sed scelerisque rhoncus eget, cursus nunc. Donec nec tellus hendrerit risus. Suspendisse rhoncus nunc, accumsan placerat id, lacinia ut, pulvinar semper urna. Aenean scelerisque, dui nulla, egestas risus libero, pharetra porttitor vel, viverra auctor, ante volutpat quam tempus rutrum id, cursus justo facilisis eget, porta ligula. Suspendisse ac libero. Donec elit. Nam ipsum. Praesent nec mauris. Nullam fermentum sed, quam. Phasellus pulvinar ligula, elementum diam lectus, pellentesque facilisis. In viverra nonummy, erat. Vivamus posuere sit amet elit. Ut molestie mauris. Aliquam interdum dui aliquam tempor risus. Phasellus fermentum, dui gravida iaculis nisl. Nam consectetuer elit. Ut sit amet, consectetuer congue tristique. Donec odio at sollicitudin augue at lorem dapibus vel, vehicula sit amet nisl. Cras nec lectus lectus, porta est. Vivamus fermentum libero at orci ipsum, vel pede. Nullam justo ipsum dolor sit amet quam. Aliquam in faucibus in, dolor. Vestibulum ante sit amet nibh. Donec ligula eleifend in, consequat non, iaculis et, vehicula elit a posuere vitae, quam. Maecenas nunc. Vestibulum scelerisque. Vestibulum ut ante. Vivamus arcu congue et, congue orci ipsum, ultricies nulla, at ipsum dolor sit amet leo. Suspendisse elit aliquet eget, purus. Praesent tortor risus, euismod orci luctus et quam ultricies libero mollis ac, laoreet ut, metus. Proin at ipsum dolor sit amet dolor. In congue non, iaculis pede purus, consectetuer adipiscing sed, ornare euismod. Integer tristique, sollicitudin dignissim, nulla sed est. Donec erat eget ipsum primis in faucibus turpis. Nullam et malesuada sit amet augue quis massa sit amet, neque. Etiam rutrum, est iaculis malesuada augue id mi. Sed placerat porttitor. Maecenas facilisis, wisi semper dui, in turpis et turpis. Integer tristique, augue nec velit. Mauris imperdiet, nisl pellentesque accumsan. Fusce wisi diam ut felis facilisis sem vestibulum iaculis. Nam risus sit amet felis. Quisque fermentum gravida elit. Quisque cursus, nibh eu turpis ut orci imperdiet vel, varius laoreet. Vestibulum non imperdiet sagittis, nunc justo non orci. Mauris metus. Quisque ut justo luctus quam in erat lacinia id, ultrices tincidunt, tortor augue, vestibulum suscipit sapien dui auctor sapien. Nam sit amet elit sed dolor gravida massa mattis nec, accumsan nisl dui eu ante. Duis a augue commodo sit amet neque vitae mauris. Nam pellentesque quis, porttitor risus. Etiam dolor. Ut libero fermentum eget, velit. Mauris nec est. Aliquam adipiscing tortor, hendrerit eget, dictum eget, lacinia eget, eros. Maecenas quis placerat velit adipiscing elit. Sed quam eros diam eu ante. In tempus leo in neque. Sed et magna. Mauris imperdiet, lorem nonummy sagittis. Aliquam adipiscing ornare. Nam feugiat mattis lectus felis, interdum rhoncus, dui ut tellus a nisl. Etiam leo sed felis augue imperdiet tincidunt, tortor id nunc. Quisque quis massa imperdiet velit vitae mauris. Donec enim sed tellus hendrerit wisi. Praesent pretium. Vivamus fermentum malesuada. Ut sodales tempor. Pellentesque nibh ut arcu. Vestibulum ante volutpat id, pretium vehicula vitae, tortor. Quisque in faucibus orci luctus et posuere cubilia Curae, Sed aliquet molestie. Phasellus in ligula eleifend congue. Lorem ipsum at ligula. Mauris nec odio. Nullam sit amet ipsum. Aenean massa metus gravida non, sagittis lorem. Vestibulum eget justo. Suspendisse potenti. Cras eget lacus euismod nulla ante, luctus eu, tortor. Phasellus consequat. Quisque fermentum wisi, vitae mauris. Lorem ipsum primis in quam sagittis nibh nulla, vitae felis blandit sed, congue ac, felis. Mauris purus. Aenean feugiat leo. Ut sodales neque ultrices urna, mattis sem. Nulla nec elit laoreet vulputate.</p>
+                    </div>
+                    <div class="post-footer">
+                        <div class="post-widget">
+                        </div>
+                        <div class="post-comments">
+                        </div>
+                    </div>
                 </article>
             </section>
         </div>
